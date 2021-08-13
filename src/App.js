e0ca55b01c4ba7952d20de1561f7482cdb0e1ba5
Ishan <ishanbaliyan@gmail.com>
2021-08-13 16:02:50 -0400

    Setup jumbotron section of page with netflix applications

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 
+import jumboData from './fixtures/jumbo'
+import Jumbotron from './components/jumbotron'
+
 export default function App() {
   return (
-    <p>Hello</p>
+
+    // Use a container to map EVERY JUMBOTRON used (same jumbotron component is used several times)
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src = {item.image} alt={item.alt}></Jumbotron.Image>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+    
   );
 }
