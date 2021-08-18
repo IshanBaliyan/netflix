549b2854efbd4c2e2a53b43c7f17c5c059a7c7b1
Ishan <ishanbaliyan@gmail.com>
2021-08-18 19:03:14 -0400

    Added Netflix accordian with footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-
-import jumboData from './fixtures/jumbo'
-import Jumbotron from './components/jumbotron'
+import { FaqsContainer } from "./containers/faqs";
+import { FooterContainer } from "./containers/footer";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
   return (
-
-    // Use a container to map EVERY JUMBOTRON used (same jumbotron component is used several times)
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src = {item.image} alt={item.alt}></Jumbotron.Image>
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-    
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+
+// Used for making it easier to organize export code (rather than taking up many lines in jumbotron.js)
+export { default as Accordion} from './accordion'
+export { default as Jumbotron } from './jumbotron';
+export { default as Footer } from './footer';
