45e9043f0162155a1d6e5cbf02f0067e580b7fa9
IshanBaliyan <ishanbaliyan@gmail.com>
2021-08-24 16:44:00 -0400

    Added header section of netflix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages"
+
 import * as ROUTES from './constants/routes';
+
 
 export default function App() {
   return (
     <Router>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+      <Route exact path='/browse'>
+        <Browse />
+      </Route>
+      <Route exact path='/signin'>
+        <Signin />
+      </Route>
+      <Route exact path='/signup'>
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
     </Router>
   );
 }
